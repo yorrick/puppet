f26c70f750e7c07fe1b9a027cb9575ea47d91449
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-11-20 11:55:34 +0100

    Updated example

--- examples/init.pp
+++ examples/init.pp
@@ -1,1 +1,7 @@
-include puppet
+
+puppet::foreman::config::puppetmaster { 'configure puppetmaster':
+    foreman_url => 'http://localhost',
+}
+
+puppet::foreman::config::puppetagent { 'configure puppetagent': }
+
