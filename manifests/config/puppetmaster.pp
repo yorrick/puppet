611db1c5627d04a0c07265ca6d609bdd9f16d948
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-11-20 14:03:22 +0100

    Fix for vardir

--- manifests/config/puppetmaster.pp
+++ manifests/config/puppetmaster.pp
@@ -1,7 +1,7 @@
 class puppet::config::puppetmaster {
 
   cron { 'clean_puppet_reports' :
-    command => "find ${::vardir}/reports/ -name \"*.yaml\" -mtime +5 -exec rm -f {} \\;",
+    command => "find ${settings::vardir}/reports/ -name \"*.yaml\" -mtime +5 -exec rm -f {} \\;",
     user    => root,
     hour    => 1,
     minute  => 0
