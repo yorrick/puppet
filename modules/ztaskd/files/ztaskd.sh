10a7fc5512e057efbdfb8055c722e10151dcb1a6
Yorrick Jansen <yorrick.jansen@gmail.com>
2013-02-12 21:41:04 -0500

    init script

--- modules/ztaskd/files/ztaskd.sh
+++ modules/ztaskd/files/ztaskd.sh
@@ -2,4 +2,5 @@
 
 echo "Script params: <$@>" >> /var/log/ztask/ztask.log
 
-source /home/webapp/virtualenvs/home_automation/bin/activate && /home/webapp/apps/home_automation/manage.py $@
+source /home/webapp/virtualenvs/home_automation/bin/activate \
+    && /home/webapp/apps/home_automation/manage.py ztaskd $@
