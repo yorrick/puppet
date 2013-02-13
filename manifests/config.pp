c5e874c8a8eba02350cca6560be98bc7b7fc8fb8
Yorrick Jansen <yorrick.jansen@gmail.com>
2013-02-12 19:43:12 -0500

    ztask init.d script

--- manifests/config.pp
+++ manifests/config.pp
@@ -188,38 +188,4 @@ file { '/etc/nginx/nginx.conf':
 
 
 
-
-package { "libzmq-dev":
-    ensure => "2.2.0+dfsg-2",
-}
-
-package { "python2.7-dev":
-    ensure => "2.7.3~rc2-2.1",
-}
-
-service { "ztaskd":
-    ensure => "running",
-    enable => "true",
-    require => [Package["libzmq-dev"], File["/var/run/ztask"], File["/var/log/ztask"], File["/etc/init.d/ztaskd"]],
-}
-
-file {'/etc/init.d/ztaskd':
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 755,
-    source  => "puppet:///modules/ztaskd/init-script.sh",
-    require => Package["libzmq-dev"],
-#    notify  => Service["ztaskd"],
-}
-
-file { "/var/run/ztask":
-    ensure => 'directory',
-    mode => '755',
-}
-
-file { "/var/log/ztask":
-    ensure => 'directory',
-    mode => '755',
-}
-
+class {'ztaskd::server':}
--- modules/ztaskd/files/init-script.sh
+++ modules/ztaskd/files/init-script.sh
@@ -18,17 +18,17 @@ DJANGO_MANAGE="/home/webapp/apps/home_automation/manage.py"
 LOGFILE_DIR="/var/log/ztask"
 [ -d "$LOGFILE_DIR" ] || { echo "no such directory: '$LOGFILE_DIR'"; exit 1; }
 LOGFILE="$LOGFILE_DIR/ztask.log"
-DAEMON="source $VIRTUALENV && $DJANGO_MANAGE --logfile \"$LOGFILE\" --noreload"
-# DAEMON="source ~/virtualenvs/home_automation/bin/activate && ~/apps/home_automation/manage.py  --logfile "$LOGFILE" --noreload --daemonize"
+DAEMON="source $VIRTUALENV && $DJANGO_MANAGE"
+DAEMON_ARGS=" --logfile \"$LOGFILE\" --noreload"
 SCRIPTNAME="/etc/init.d/${NAME}"
 PIDFILE=/var/run/$NAME.pid
 UMASK=022
-ZTASKD_USER="WEBAPP"
+ZTASKD_USER="webapp"
 
 # Exit if the virtualenv is not installed
-[ -x "VIRTUALENV" ] || exit 0
+[ -r "$VIRTUALENV" ] || exit 0
 # Exit if the virtualenv is not installed
-[ -x "DJANGO_MANAGE" ] || exit 0
+[ -x "$DJANGO_MANAGE" ] || exit 0
 
 # Load the VERBOSE setting and other rcS variables
 . /lib/init/vars.sh
@@ -46,19 +46,16 @@ do_start()
    #   0 if daemon has been started
    #   1 if daemon was already running
    #   2 if daemon could not be started
-   start-stop-daemon --start --background --quiet --pidfile $PIDFILE --exec $DAEMON1 \
-      --chuid $DELUGED_USER --user $ZTASKD_USER --umask $UMASK --test > /dev/null
+   echo "DAEMON: $DAEMON"
+   start-stop-daemon --start --background --quiet --pidfile $PIDFILE --exec '$DAEMON' \
+      --chuid $ZTASKD_USER --user $ZTASKD_USER --umask $UMASK --test > /dev/null
    RETVAL="$?"
    [ "$RETVAL" = "0" ] || return 1
 
-   start-stop-daemon --start --background --quiet --pidfile $PIDFILE1 --make-pidfile --exec $DAEMON1 \
-      --chuid $DELUGED_USER --user $ZTASKD_USER --umask $UMASK -- $DAEMON1_ARGS
-   RETVAL1="$?"
-        sleep 2
-   start-stop-daemon --start --background --quiet --pidfile $PIDFILE2 --make-pidfile --exec $DAEMON2 \
-      --chuid $DELUGED_USER --user $ZTASKD_USER --umask $UMASK -- $DAEMON2_ARGS
-   RETVAL2="$?"
-   [ "$RETVAL1" = "0" -a "$RETVAL2" = "0" ] || return 2
+   start-stop-daemon --start --background --quiet --pidfile $PIDFILE1 --make-pidfile --exec \"$DAEMON\" \
+      --chuid $ZTASKD_USER --user $ZTASKD_USER --umask $UMASK -- $DAEMON_ARGS
+   RETVAL="$?"
+   [ "$RETVAL" = "0" ] || return 2
 }
 
 #
@@ -72,20 +69,18 @@ do_stop()
    #   2 if daemon could not be stopped
    #   other if a failure occurred
 
-   start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --user $ZTASKD_USER --pidfile $PIDFILE2
-   RETVAL2="$?"
-   start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --user $ZTASKD_USER --pidfile $PIDFILE1
-   RETVAL1="$?"
-   [ "$RETVAL1" = "2" -o "$RETVAL2" = "2" ] && return 2
+   start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --user $ZTASKD_USER --pidfile $PIDFILE
+   RETVAL="$?"
+   [ "$RETVAL" = "2" ] && return 2
 
-   rm -f $PIDFILE1 $PIDFILE2
+   rm -f $PIDFILE
 
-   [ "$RETVAL1" = "0" -a "$RETVAL2" = "0" ] && return 0 || return 1
+   [ "$RETVAL" = "0" ] && return 0 || return 1
 }
 
 case "$1" in
   start)
-   [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME1"
+   [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
    do_start
    case "$?" in
       0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
@@ -93,7 +88,7 @@ case "$1" in
    esac
    ;;
   stop)
-   [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME1"
+   [ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
    do_stop
    case "$?" in
       0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
@@ -101,7 +96,7 @@ case "$1" in
    esac
    ;;
   restart|force-reload)
-   log_daemon_msg "Restarting $DESC" "$NAME1"
+   log_daemon_msg "Restarting $DESC" "$NAME"
    do_stop
    case "$?" in
      0|1)
