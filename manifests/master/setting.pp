f1a2068547ad5e7ab1150cb4eec5019a0179318e
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-06-04 12:14:13 +0200

    Added define for puppet master settings

---
+++ manifests/master/setting.pp
@@ -0,0 +1,33 @@
+define puppet::master::setting( $ensure = 'present', $value = undef ) {
+
+  Augeas {
+    lens    => 'Puppet.lns',
+    incl    => '/etc/puppet/puppet.conf',
+    context => '/files/etc/puppet/puppet.conf/master',
+    notify  => Service['puppet'],
+  }
+
+  case $ensure {
+    'absent': {
+      augeas { "puppet::master::${title}":
+        onlyif  => "match ${title} size > 0",
+        changes => "rm ${title}",
+      }
+    }
+    'present': {
+      if ($value == undef) or (! is_string($value)) {
+        fail("Puppet::Master::Setting[${title}]: required parameter value must be a non-empty string")
+      }
+      else {
+        augeas { "puppet::master::${title}":
+          onlyif  => "match ${title}[. = ${value}] size == 0",
+          changes => "set ${title} ${value}",
+        }
+      }
+    }
+    default: {
+      fail("Puppet::Master::Setting[${title}]: parameter ensure must be present or absent")
+    }
+  }
+
+}
