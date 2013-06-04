0b1afd69e34c91c7676304bb6239d261169aead9
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-06-04 12:13:56 +0200

    Removed foreman reporting config

--- manifests/foreman/config/puppetmaster.pp
+++ manifests/foreman/config/puppetmaster.pp
@@ -10,18 +10,6 @@ define puppet::foreman::config::puppetmaster($foreman_url = undef) {
     fail("Reporting::Foreman::Config::Puppetmaster[$foreman_url]: foreman_url must be a valid URL")
   }
 
-  augeas { 'configure-foreman-reporting-for-puppetmaster':
-    lens    => 'Puppet.lns',
-    incl    => '/etc/puppet/puppet.conf',
-    context => '/files/etc/puppet/puppet.conf/master',
-    changes => [
-      'set reports "store, foreman"'
-    ],
-    onlyif  => 'match reports[.="store, foreman"] size == 0',
-    require => File['/etc/puppet/puppet.conf'],
-    notify  => Service['puppet']
-  }
-
   file { '/usr/lib/ruby/site_ruby/1.8/puppet/reports/foreman.rb' :
     ensure  => file,
     owner   => root,
