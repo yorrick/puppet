236c7a9834ff6b26a6e91d12936ba0f4376e90a5
Kristof Willaert <kristof.willaert@cegeka.be>
2014-02-14 11:22:46 +0100

    Better spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,8 @@
-require 'puppet'
 require 'rspec-puppet'
-require 'tmpdir'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.before :each do
-    # Create a temporary puppet confdir area and temporary site.pp so
-    # when rspec-puppet runs we don't get a puppet error.
-    @puppetdir = Dir.mktmpdir
-    manifestdir = File.join(@puppetdir, "manifests")
-    Dir.mkdir(manifestdir)
-    FileUtils.touch(File.join(manifestdir, "site.pp"))
-    Puppet[:confdir] = @puppetdir
-  end
-
-  c.after :each do
-    FileUtils.remove_entry_secure(@puppetdir)
-  end
-
-  c.module_path = File.join(File.dirname(__FILE__), 'fixtures/modules')
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
